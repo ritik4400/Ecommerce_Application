c2ee424606a606cab295dd12eb1c31b16706ebe8
Ritik Trivedi <ritiktrivedi902@gmail.com>
2024-12-02 20:37:19 +0530

    bcrypt added in model

--- backend/src/model/userModel.js
+++ backend/src/model/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+var bcrypt = require('bcryptjs');
 
 const userSchema =new Schema({
     firstName: {
@@ -27,6 +28,20 @@ const userSchema =new Schema({
         enum:[1 , -1] //1 active , -1 deleted
     }
 } , {timestamps: true})
+
+
+userSchema.pre('save', async function (next) {
+   
+    if(!this.isModified('password')) return next();
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt)
+    next();
+  });
+
+  userSchema.methods.comparePassword = async function (password) {
+    return await bcrypt.compare(password, this.password);
+  
+  }
 
 const User = mongoose.model('User', userSchema);
 module.exports = User;
